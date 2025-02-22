253a00ad77a2acb71414dbcc69261d8171cdf648
Daniil Pokataev <daniil.pokataev@ucdconnect.ie>
2025-02-22 22:04:02 +0000

    added Launcher class to help with building .jar file

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
 
-public static void main(String[] args) {
-    Application.launch(HexMap.class, args);
-    System.out.println(HexMap.hexagons[0][0].getX());
-    System.out.println(HexMap.hexagons[0][0].getY());
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(HexMap.class, args);
+        System.out.println(HexMap.hexagons[0][0].getX());
+        System.out.println(HexMap.hexagons[0][0].getY());
+    }
 }
