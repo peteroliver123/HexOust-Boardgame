8c9b3ecbe5509b934b7381bf8f915e2a877480c8
peteroliver123 <Brackenp18@gmail.com>
2025-03-30 17:36:26 +0100

    Forgot to add point file

---
+++ main/src/Point.java
@@ -0,0 +1,52 @@
+/*
+This is a helper class which describes a point on the pane with an x co-ordinate
+and a y co-ordinate. The function error checks the users input for new points
+ */
+public class Point {
+    /*Variables */
+    private final double x;
+    private final double y;
+
+    /*Constructor */
+    public Point(double x, double y){
+        /*Error Checking */
+        if(x < 0 || y < 0){//Check if user input is off-screen
+            throw new IllegalArgumentException("center of Hexagon cannot be outside screen");
+        }
+
+        this.x = x;
+        this.y = y;
+    }
+
+    /*Getter Methods */
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void coordinateCheck(){
+        if(x < 0 || y < 0){
+            throw new IllegalArgumentException("Positions of Hexagons must be within the board!");
+        }
+        /*If x is 0 - 6 goes up to x + 6*/
+        if(x > 12){
+            throw new IllegalArgumentException("There are not more than 12 columns of hexagons");
+        }
+
+        if(x <= 6){
+            if(y > x + 6){
+                throw new IllegalArgumentException("For first 7 columns of hexagons, where i is the column, there is no more than i + 6 rows");
+            }
+        }
+        else {
+            if(y > 18 - x){
+                throw new IllegalArgumentException("For columns 7 to 12, where i is the column, there is no more than 18 - i rows");
+            }
+        }
+    }
+
+}
+
